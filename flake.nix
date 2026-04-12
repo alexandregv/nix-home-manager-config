21fc0650518d0ca534fcf224aef210da10eb09eb
Reach <reach@42paris.fr>
2026-04-12 19:59:39 +0200

    feat!: use 25.11 channels

--- flake.nix
+++ flake.nix
@@ -2,12 +2,10 @@
   description = "Home Manager configuration of Reach";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    #nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
-      #url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
