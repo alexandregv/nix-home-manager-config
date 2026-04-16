a1978ae4c01ae9a0708f34ff505066fb63e460f8
Reach <reach@42paris.fr>
2026-04-17 00:10:09 +0200

    feat(progs): add atuin

--- home.nix
+++ home.nix
@@ -53,4 +53,14 @@
   programs.bash = {
     enable = true;
   };
+
+  programs.atuin = {
+    enable = true;
+    flags = [ "--disable-up-arrow" ];
+    settings = {
+      filter_mode = "global";
+      style = "full";
+      inline_height = 0;
+    };
+  };
 }
