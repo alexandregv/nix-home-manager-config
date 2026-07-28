36422346c3b079bb6453a487507f63e19318ff87
Reach <reach@42paris.fr>
2026-07-29 00:32:05 +0200

    feat: update to nixpkgs+HM 26.05

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "Home Manager configuration of Reach";
 
   inputs = {
-    nixpkgs.url  = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url  = "github:nixos/nixpkgs/nixos-26.05";
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager-src = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       flake = false;
     };
 
